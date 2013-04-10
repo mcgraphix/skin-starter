d0b42910d25ec7d08e36fe0c84bec63e8ebc1739
kristoferjoseph <kristoferjoseph@gmail.com>
2013-04-10 15:37:15 -0700

    Updating example skin starter grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,71 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
+    var getStylusPathData = function(grunt) {
+            var controlsPath = grunt.file.expand('tmp/src/controls/**/src/mixins'),
+                utilsPath = grunt.file.expand('tmp/src/utils/**/src/mixins'),
+                variablesPath = 'test/fixtures'
+            pathData = [variablesPath];
+
+            return pathData.concat(controlsPath, utilsPath);
+        };
+
     // Project configuration.
     grunt.initConfig({
         // Task configuration.
+        pkg: grunt.file.readJSON('package.json'),
         gruntfile: {
             src: 'Gruntfile.js'
         },
+
+        topcoat: {
+            download: {
+                options: {
+                    srcPath: 'tmp/src/',
+                    repos: '<%= pkg.topcoat %>'
+                }
+            }
+        },
+
+        unzip: {
+            controls: {
+                src: 'tmp/src/controls/*.zip',
+                dest: 'tmp/src/controls'
+            },
+            utils: {
+                src: 'tmp/src/utils/*.zip',
+                dest: 'tmp/src/utils'
+            }
+        },
+
+        clean: {
+            tmp: ['tmp'],
+            zip: ['tmp/src/*.zip', 'tmp/src/controls/*.zip', 'tmp/src/skins/*.zip', 'tmp/src/utils/*.zip']
+        },
+
+        stylus: {
+            compile: {
+                options: {
+                    paths: getStylusPathData(grunt),
+                    import: ['EXAMPLE'],
+                    compress: false
+                },
+                files: {
+                    'release/EXAMPLE.css': ['src/copyright.styl', 'src/EXAMPLE.styl']
+                }
+            }
+        },
+
+        cssmin: {
+            minify: {
+                expand: true,
+                cwd: 'release/css/',
+                src: ['*.css', '!*.min.css'],
+                dest: 'release/css/',
+                ext: '.min.css'
+            }
+        },
+
         stylus: {
             compile: {
                 options: {
@@ -50,8 +109,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-contrib-jade');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-topcoat');
+    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task.
-    grunt.registerTask('default', ['stylus', 'jade', 'nodeunit']);
-
+    grunt.registerTask('default', ['clean', 'topcoat', 'build']);
+    grunt.registerTask('build', ['stylus', 'cssmin', 'jade', 'nodeunit']);
 };
